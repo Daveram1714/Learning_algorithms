f303fa091aa9d31ed495dba766eda6dbe35e74e7
Daveram1714 <daveram4529a@gmail.com>
2026-02-20 07:04:26 +0530

    quick sort algorithm

--- insersesonShort.js
+++ insersesonShort.js
@@ -2,11 +2,16 @@ function inserseonShort(arr){
     for(let i = 1;i<arr.length;i++){
         let numberToInsert = arr[i]
         let j = i-1
+        console.log('j', j)
         while(j>= 0 && arr[j] > numberToInsert){
+            console.log('arr[j]', arr[j])
             arr[j+1] = arr[j]
+            console.log('j+1 = arr[j]', j+1 , arr[j])
             j = j-1
+            console.log('j-1,j', j-1,j)
         }
         arr[j+1] = numberToInsert
+        console.log('arr[j+1],numberToInsert', arr[j+1],numberToInsert)
     }
 }
 
